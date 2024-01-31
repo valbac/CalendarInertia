f9e4ed23f80cb76b771dcdd7e6ebfe8f121325ee
valbac <eztore1412@gmail.com>
2024-01-31 16:35:42 +0100

    Stable - The Reminder is now only set when clicking on the floating action button and not when checking the switch for the reminder. - The Notification is now clickable. It launches the App and clears the notification when clicked on.

--- app/src/main/java/com/valbac/calendarinertia/core/AlarmReceiver.kt
+++ app/src/main/java/com/valbac/calendarinertia/core/AlarmReceiver.kt
@@ -1,10 +1,12 @@
 package com.valbac.calendarinertia.core
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.valbac.calendarinertia.MainActivity
 import com.valbac.calendarinertia.R
 import kotlin.random.Random
 class AlarmReceiver : BroadcastReceiver () {
@@ -14,6 +16,17 @@ class AlarmReceiver : BroadcastReceiver () {
                 .setContentText(message)
                 .setContentTitle("Task Reminder")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(
+                    PendingIntent.getActivity(
+                        it,
+                        0,
+                        Intent(it, MainActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        },
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    )
+                )
+                .setAutoCancel(true)
                 .build()
         }
         context?.getSystemService(NotificationManager::class.java)?.notify(
